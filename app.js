3716ed3d18ce6820f94942256cd6a5154e55cb9b
Paula Ramirez <pj.ramirez10@uniandes.edu.co>
2018-03-09 08:02:17 -0500

    Express static

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'p1-client/')));
+app.use(express.static(path.join(__dirname, 'p1-client/build')));
 
 app.use('/api', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
