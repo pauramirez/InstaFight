c7b838bce963b4dfcffa7fb1080b2edfe282497e
Paula Ramirez <pj.ramirez10@uniandes.edu.co>
2018-03-09 08:05:17 -0500

    users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'p1-client/build')));
 
 app.use('/api', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
