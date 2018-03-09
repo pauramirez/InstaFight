3801ac237a875bce25d1b9405a944469f3087504
Paula Ramirez <pj.ramirez10@uniandes.edu.co>
2018-03-09 01:39:08 -0500

    Express routing

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -20,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'p1-client/build')));
 
 app.use('/', index);
 app.use('/users', users);
